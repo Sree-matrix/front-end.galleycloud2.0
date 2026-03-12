cdfc37ff395cace478865b9babf3cb2e5ec2dfd3
sreevenkateshr <sreevenkateshr@gmail.com>
2026-03-13 02:06:08 +0530

    base btn fixed

--- src/components/buttons/base.tsx
+++ src/components/buttons/base.tsx
@@ -1,44 +1,38 @@
 import Link from "next/link";
-import { Button, ButtonProps, SvgIconTypeMap } from "@mui/material";
+import { Button, CircularProgress, ButtonProps, SvgIconTypeMap } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
-
-declare module "@mui/material/Button" {
-  interface ButtonPropsVariantOverrides {
-    whitebtn: true;
-  }
-}
 
 export interface BaseButtonProps extends ButtonProps {
   LeftIcon?: OverridableComponent<SvgIconTypeMap<object, "svg">>;
-  children: React.ReactNode;
+  children: string | React.ReactNode;
   RightIcon?: OverridableComponent<SvgIconTypeMap<object, "svg">>;
   loading?: boolean;
   rounded?: boolean;
 }
 
 export const BaseButton: React.FC<BaseButtonProps> = (props) => {
-  const {
-    LeftIcon,
-    children,
-    RightIcon,
-    variant = "contained",
-    loading,
-    href,
-    rounded,
-    ...rest
-  } = props;
+  const { LeftIcon, children, RightIcon, variant, loading, href, rounded, ...rest } = props;
 
   return (
     <Button
-      variant={variant}
+      variant={variant ?? "contained"}
       {...(href && { LinkComponent: Link, href })}
       {...rest}
-      sx={{
-        borderRadius: rounded ? "10rem" : undefined,
-        ...rest.sx,
-      }}
+      sx={{ borderRadius: rounded ? "10rem" : undefined, ...rest.sx }}
     >
+      {LeftIcon && (
+        <LeftIcon sx={{ width: "1rem", height: "1rem", margin: "auto 0.5rem auto -0.25rem" }} />
+      )}
       {children}
+      {loading && (
+        <CircularProgress
+          size="1rem"
+          sx={{ marginLeft: "0.625rem", ...(rest.disabled && { color: "#00000033" }) }}
+        />
+      )}
+      {RightIcon && (
+        <RightIcon sx={{ width: "1rem", height: "1rem", margin: "auto -0.25rem auto 0.5rem" }} />
+      )}
     </Button>
   );
 };
